ba7679ab8d8fe270c1e94df879b34c50fb9fc90d
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2021-12-12 18:07:01 +0100

    Added automatic password generation

--- mysql-install-centos8.sh
+++ mysql-install-centos8.sh
@@ -16,7 +16,7 @@
 # REV LIST:
 # DATE: 18-12-2020
 # BY: ALBERT VALBUENA
-# MODIFICATION: Creation
+# MODIFICATION: 12-12-2021
 #
 #
 # set -n # Uncomment to check your syntax, without execution.
@@ -24,20 +24,11 @@
 # # the shell script will not execute!
 
 ##########################################################
-########### DEFINE FILES AND VARIABLES HERE ##############
-##########################################################
-
-
-
-##########################################################
-############### DEFINE FUNCTIONS HERE ####################
-##########################################################
-
-
-
-##########################################################
 ################ BEGINNING OF MAIN #######################
 ##########################################################
+
+# Let's update CentOS local repositories on this box.
+dnf update -y
 
 # Let's install MySQL database.
 dnf install -y mysql-server mysql
@@ -48,29 +39,27 @@ systemctl enable mysqld
 # Start up MySQL
 systemctl start mysqld
 
+# Install pwgen to automatically generate passwords
+dnf install -y pwgen
+
+# Define the DB root password and export it as a variable to make it available for the expect script. Plus write it on the root directory.
+DB_ROOT_PASSWORD=$(pwgen 32 --secure --numerals --capitalize) && export DB_ROOT_PASSWORD && echo $DB_ROOT_PASSWORD >> /root/db_root_pwd.txt
+
 # Install Expect so the MySQL secure installation process can be automated.
 dnf install -y expect
 
-# Make the hideous 'safe' install for MySQL.
-##########################################################
-################### IMPORTANT !!!! #######################
-##########################################################
-
-# Change the password found below!!!
-# Not changing the password found on this script on the internet is a huge security risk.
-
-
 SECURE_MYSQL=$(expect -c "
 set timeout 10
+set DB_ROOT_PASSWORD "$DB_ROOT_PASSWORD"
 spawn mysql_secure_installation
 expect \"Press y|Y for Yes, any other key for No:\"
 send \"y\r\"
 expect \"Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:\"
-send \"2\r\"
+send \"0\r\"
 expect \"New password:\"
-send \"QB_e-6qUe_vs521\r\"
+send \"$DB_ROOT_PASSWORD\r\"
 expect \"Re-enter new password:\"
-send \"QB_e-6qUe_vs521\r\"
+send \"$DB_ROOT_PASSWORD\r\"
 expect \"Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :\"
 send \"Y\r\"
 expect \"Remove anonymous users?\"
@@ -85,6 +74,12 @@ expect eof
 ")
 
 echo "$SECURE_MYSQL"
+
+# Display the location of the generated root password for MySQL
+echo "Your DB_ROOT_PASSWORD is written on this file /root/db_root_pwd.txt"
+
+# No one but root can read this file. Read only permission.
+chmod 400 /root/db_root_pwd.txt
 
 # Sources:
 # https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-centos-8
